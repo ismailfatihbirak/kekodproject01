ad569799aa640b8f55d18c3651715ce60f37fabf
ismailfatihbirak <ismailloodos@gmail.com>
2024-08-28 21:16:45 +0300

    bug fix

--- app/src/main/java/com/isodev/kekodproject01/HomeFragment.kt
+++ app/src/main/java/com/isodev/kekodproject01/HomeFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.CompoundButton
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.isodev.kekodproject01.databinding.FragmentHomeBinding
@@ -32,13 +33,39 @@ class HomeFragment : Fragment() {
         val navController = findNavController()
         binding.bottomNav.setupWithNavController(navController)
 
-        binding.bottomNav.menu.add(0, R.id.homeFragment, 0, getString(R.string.home)).setIcon(R.drawable.baseline_home_24)
+        binding.bottomNav.menu.add(0, R.id.homeFragment, 0, getString(R.string.home))
+            .setIcon(R.drawable.baseline_home_24)
 
-        setupSwitch(binding.sw1, 1, R.id.switchFragment1, R.string.switch_1, R.drawable.baseline_fort_24)
-        setupSwitch(binding.sw2, 2, R.id.switchFragment2, R.string.switch_2, R.drawable.baseline_male_24)
-        setupSwitch(binding.sw3, 3, R.id.switchFragment3, R.string.switch_3, R.drawable.baseline_rocket_24)
-        setupSwitch(binding.sw4, 4, R.id.switchFragment4, R.string.switch_4, R.drawable.baseline_nightlight_24)
-        setupSwitch(binding.sw5, 5, R.id.switchFragment5, R.string.switch_5, R.drawable.baseline_scuba_diving_24)
+        setupSwitch(
+            binding.sw1,
+            R.id.switchFragment1,
+            R.string.switch_1,
+            R.drawable.baseline_fort_24
+        )
+        setupSwitch(
+            binding.sw2,
+            R.id.switchFragment2,
+            R.string.switch_2,
+            R.drawable.baseline_male_24
+        )
+        setupSwitch(
+            binding.sw3,
+            R.id.switchFragment3,
+            R.string.switch_3,
+            R.drawable.baseline_rocket_24
+        )
+        setupSwitch(
+            binding.sw4,
+            R.id.switchFragment4,
+            R.string.switch_4,
+            R.drawable.baseline_nightlight_24
+        )
+        setupSwitch(
+            binding.sw5,
+            R.id.switchFragment5,
+            R.string.switch_5,
+            R.drawable.baseline_scuba_diving_24
+        )
     }
 
     private fun setAllOtherSwitchesEnabled(enabled: Boolean) {
@@ -48,6 +75,7 @@ class HomeFragment : Fragment() {
         binding.sw4.isEnabled = enabled
         binding.sw5.isEnabled = enabled
     }
+
     private fun resetAllOtherSwitches() {
         binding.sw1.isChecked = false
         binding.sw2.isChecked = false
@@ -55,10 +83,24 @@ class HomeFragment : Fragment() {
         binding.sw4.isChecked = false
         binding.sw5.isChecked = false
     }
-    private fun setupSwitch(switch: CompoundButton, menuId: Int, fragmentId: Int, titleResId: Int, iconResId: Int) {
+    private fun setupSwitch(
+        switch: CompoundButton,
+        fragmentId: Int,
+        titleResId: Int,
+        iconResId: Int
+    ) {
         switch.setOnCheckedChangeListener { _, isChecked ->
             if (isChecked) {
-                binding.bottomNav.menu.add(menuId, fragmentId, 1, getString(titleResId)).setIcon(iconResId)
+                if (binding.bottomNav.menu.size() < 5) {
+                    binding.bottomNav.menu.add(1, fragmentId, 1, getString(titleResId)).setIcon(iconResId)
+                } else {
+                    Toast.makeText(
+                        binding.root.context,
+                        getString(R.string.toast_message),
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    switch.isChecked = false
+                }
             } else {
                 binding.bottomNav.menu.removeItem(fragmentId)
             }
