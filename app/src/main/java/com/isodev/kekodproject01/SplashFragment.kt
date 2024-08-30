ff0a1e341624e6aa588cbeaa905a49f4d49ce542
ismailfatihbirak <ismailloodos@gmail.com>
2024-08-30 12:59:02 +0300

    added Splash and animations

--- app/src/main/java/com/isodev/kekodproject01/SplashFragment.kt
+++ app/src/main/java/com/isodev/kekodproject01/SplashFragment.kt
@@ -1,10 +1,15 @@
 package com.isodev.kekodproject01
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
+import androidx.navigation.NavOptions
+import androidx.navigation.fragment.findNavController
 import com.isodev.kekodproject01.databinding.FragmentSplashBinding
 
 class SplashFragment : Fragment() {
@@ -15,5 +20,22 @@ class SplashFragment : Fragment() {
     ): View? {
         binding = FragmentSplashBinding.inflate(inflater, container, false)
         return binding.root
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val imageView = binding.imageView2
+        val fadeIn = AnimationUtils.loadAnimation(requireContext(), R.anim.fade_in)
+        imageView.startAnimation(fadeIn)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val navOptions = NavOptions.Builder()
+                .setEnterAnim(R.anim.enter_from_right)
+                .setExitAnim(R.anim.exit_to_left)
+                .setPopUpTo(R.id.splashFragment, true) // Remove splash from back stack
+                .build()
+
+            findNavController().navigate(R.id.action_splashFragment_to_homeFragment, null, navOptions)
+        }, fadeIn.duration)
     }
 }
